265d724a09210baa6a068fabfe9c28ca6ba8f238
Jose Francisco Martinez Rios <josemartzrios14@gmail.com>
2026-04-15 16:23:40 -0700

    feat(landing): add Footer and assemble full landing page

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -2,14 +2,18 @@ import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Benefits from '../components/Benefits'
 import Pricing from '../components/Pricing'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Nav />
-      <Hero />
-      <Benefits />
-      <Pricing />
-    </main>
+      <main>
+        <Hero />
+        <Benefits />
+        <Pricing />
+      </main>
+      <Footer />
+    </>
   )
 }
