2b7191e0873ba3ae62948ae30025bcf0ca398ea0
Jose Francisco Martinez Rios <josemartzrios14@gmail.com>
2026-04-15 16:22:59 -0700

    feat(landing): add Hero component

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,10 +1,11 @@
 import Nav from '../components/Nav'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <main>
       <Nav />
-      <h1 className="p-8">SyqueX</h1>
+      <Hero />
     </main>
   )
 }
