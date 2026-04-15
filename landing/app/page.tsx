7da72ae798cfc2bbecd9d66dc7b29e9e1d2efa54
Jose Francisco Martinez Rios <josemartzrios14@gmail.com>
2026-04-15 16:23:09 -0700

    feat(landing): add Benefits component

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
+import Benefits from '../components/Benefits'
 
 export default function Home() {
   return (
     <main>
       <Nav />
       <Hero />
+      <Benefits />
     </main>
   )
 }
